b95d2db0b5922d1640fa5d7db7897bba4a5eaba4
Null <null.jared@gmail.com>
2017-12-25 14:42:01 -0800

    updates

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -33,7 +33,7 @@ const headerStyle = {
       fontSize: '80px',
       fontFamily: 'Delicious',
       padding: '5px 20px 0px 5px',
-      color: 'rgba(93, 32, 32, 1)',
+      color: 'rgb(144, 26, 26)',
       lineHeight: '1.14'
 }
 const headerBodySeparatorStyle = {
@@ -121,40 +121,100 @@ const Media = () => (
 const Projects = () => (
   <div>
     <h1 style={sectionHeaderStyle}>Projects</h1>
+    <div style={projectsMainStyle}>
+      <h1>Web Apps</h1>
+
+      <ol>
+        <li><a href='http://www.pushvote.com'>PushVote</a></li>
+        <li><a href='http://www.blackhat.io'>BlackHat</a></li>
+        <li><a href='http://www.whitehat.io'>WhiteHat</a></li>
+      </ol>
+      <h1>Add ONs / Extentions</h1>
+      <ol>
+        <li><a href="https://github.com/Jnull/noclick">Firefox - NoClick</a></li>
+      </ol>
+      <h1>Hardware</h1>
+      <ol>
+        <li>LED Illuminator bypass meter maid parking meters.</li>
+      </ol>
+      <h1>Games</h1>
+      <a href="http://www.geshl2.com">Golden Eye Source</a>
+      <ol>
+        <li>GE:S - Give users Dev Tags on per server (DNS)</li>
+        <li>GE:S - Generate Hashed UserID from SteamID (C)</li>
+        <li><a href="https://github.com/Jnull/ges-gameplay-TeamArsenal">GE:S - Created TeamArsenal</a></li>
+      </ol>
+      <h1>Bug Bounty</h1>
+      <ol>
+        <li><a href="https://www.123formbuilder.com/security-acknowledgements.htm">123formbuilder.com (XSS)</a></li>
+        <li>Department of Justice (XSS)</li>
+      </ol>
+    </div>
   </div>
 )
+
+const  projectsMainStyle = {
+  marginLeft: '35px'
+}
 
 const WebApps = () => (
   <div>
     <h1 style={sectionHeaderStyle}>WebApps</h1>
+    <ol>
+      <li><a href='http://www.pushvote.com'>PushVote</a></li>
+      <li><a href='http://www.blackhat.io'>BlackHat</a></li>
+      <li><a href='http://www.whitehat.io'>WhiteHat</a></li>
+    </ol>
   </div>
 )
 
 const Resources = () => (
   <div>
     <h1 style={sectionHeaderStyle}>Resources</h1>
+    <div style={resourcesMainStyle}>
+      <h1>Books</h1>
+      <ol>
+        <li>Web Application Security Handbook</li>
+        <li>Hacker's Playbook</li>
+        <li>Hacking Exposed</li>
+      </ol>
+    </div>
   </div>
 )
+
+const resourcesMainStyle  = {
+  marginLeft: '35px'
+}
+
 
 const About = () => (
   <div>
     <h1 style={sectionHeaderStyle}>About</h1>
+    <div style={aboutMainStyle}>
+      <p>Thrill Seeker, Risk Taker, Knowledge Aquirer</p>
+      <p>Interested in all advancement of technology, against planned obsolescence.</p>
+    </div>
   </div>
 )
+
+const aboutMainStyle = {
+marginLeft: '35px'
+}
 
 const Contact = () => (
   <div>
     <h1 style={sectionHeaderStyle}>Contact</h1>
+    <ol>
+      <li>Linkedin</li>
+      <li>contactme@jarednull.com</li>
+    </ol>
   </div>
 )
-
-
 
 const menuNavListItemStyle =  {
 height: '30px',
 borderRadius: '6px 0px 6px 0px',
 }
-
 
 const menuNavStyle =  {
 display: 'flex',
